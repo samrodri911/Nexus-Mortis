40fd712f057b71b90828347794e9f4801a371cf9
Samuel <samrosolar2007@gmail.com>
2026-06-05 12:11:19 -0500

    Estructura base

---
+++ android/app/src/main/kotlin/com/example/nexus_mortis/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nexus_mortis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audio_session
+import isar_flutter_libs
+import just_audio
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+}
